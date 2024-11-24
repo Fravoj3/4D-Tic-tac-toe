1c80f6595b4b6950658f0ee12a665c3278e9739c
Fravoj Envy <fravoj3@gmail.com>
2024-11-24 23:15:13 +0100

    smutně mnoho hran

---
+++ Points.py
@@ -0,0 +1,139 @@
+from ursina import *
+
+class Points:
+    class Line:
+        def __init__(self, A, B, points, distanceBetweenSpheres):
+            self.pointA = A
+            self.pointB = B
+            self.points = points
+            vec1 = Vec3((A[0]-1)*distanceBetweenSpheres, (A[1]-1)*distanceBetweenSpheres, (A[2]-1)*distanceBetweenSpheres)
+            vec2 = Vec3((B[0]-1)*distanceBetweenSpheres, (B[1]-1)*distanceBetweenSpheres, (B[2]-1)*distanceBetweenSpheres)
+            self.object = Entity(model=Mesh(
+            vertices=[vec1, vec2],  # The two endpoints of the line
+            mode='line',  # Render the mesh as a line¨
+            thickness=0.006,  # The thickness of the line
+            ),color=color.rgb(208, 208, 208))
+            #self.object = Entity(model=Pipe(path=(vec1, vec2), cap_ends=False, thicknesses=((0.01, 0.01))), color=color.gray)
+
+
+    class Point:
+        def __init__(self, position: tuple, sphereRadius: int):
+            self.position = position
+            self.sphereRadius = sphereRadius
+            self.object = Entity(model='sphere', color=color.orange, position=(position[0], position[1], position[2]), scale=(sphereRadius, sphereRadius, sphereRadius))
+            self.PositionMatrix4d = [[position[0]], [position[1]], [position[2]], [position[3]]]
+
+        def setObjectPositionByMatrix(self, matrix):
+            self.object.x = matrix[0][0]
+            self.object.y = matrix[1][0]
+            self.object.z = matrix[2][0]
+
+    def __init__(self, size, distanceBetweenSpheres, sphereRadius):
+        self.sphereRadius = sphereRadius
+        self.distanceBetweenSpheres= distanceBetweenSpheres
+        self.points = []
+        self.lines = {}
+        for i in range(size):
+            self.points.append([])
+            for j in range(size):
+                self.points[i].append([])
+                for k in range(size):
+                    self.points[i][j].append([])
+                    for l in range(size):
+                        position = ((i-1)*distanceBetweenSpheres, (j-1)*distanceBetweenSpheres, (k-1)*distanceBetweenSpheres, (l-1)*distanceBetweenSpheres)
+                        self.points[i][j][k].append(Points.Point(position, sphereRadius))
+                        isI = i < size - 1
+                        isJ = j < size - 1
+                        isK = k < size - 1
+                        isL = l < size - 1
+                        # Nediagonální směry
+                        if isI:
+                            self.lines[((i, i+1), (j, j), (k, k), (l, l))] = Points.Line((i, j, k, l), (i+1, j, k, l),self.points, distanceBetweenSpheres)
+                        if isJ:
+                            self.lines[((i, i), (j, j+1), (k, k), (l, l))] = Points.Line((i, j, k, l), (i, j+1, k, l),self.points, distanceBetweenSpheres)
+                        if isK:
+                            self.lines[((i, i), (j, j), (k, k+1), (l, l))] = Points.Line((i, j, k, l), (i, j, k+1, l),self.points, distanceBetweenSpheres)
+                        if isL:
+                            self.lines[((i, i), (j, j), (k, k), (l, l+1))] = Points.Line((i, j, k, l), (i, j, k, l+1),self.points, distanceBetweenSpheres)
+        
+        # Přidání diagonálních směrů  
+        added_set = set()
+        startingPointSet = set()
+
+        def addLines(vector, startingPoint):
+            if (vector[0]*-1, vector[1]*-1, vector[2]*-1, vector[3]*-1) in added_set:
+                return
+            added_set.add(tuple(vector))
+            for i in range(1, size):
+                newPoint = [startingPoint[0] + vector[0]*i, startingPoint[1] + vector[1]*i, startingPoint[2] + vector[2]*i, startingPoint[3] + vector[3]*i]
+                self.lines[(tuple(startingPoint), tuple(newPoint))] = Points.Line(startingPoint, newPoint, self.points, distanceBetweenSpheres)
+
+        def permutate(vec, function, val: list = []):
+            if len(val) == 4:
+                function(val)
+                return
+            else:
+                set_vec = set(vec)
+                for i in set_vec:
+                    copyedVec = vec.copy()
+                    copyedVec.remove(i)
+                    copyedVal = val.copy()
+                    copyedVal.append(i)
+                    permutate(copyedVec, function, copyedVal)
+        
+        def getComponentValue(value):
+            if value == 1:
+                return 0
+            if value == -1:
+                return size-1
+            if value == 0:
+                return 0
+            
+        def addDiagonalLines(vector):
+            startingPoint = (getComponentValue(vector[0]), getComponentValue(vector[1]), getComponentValue(vector[2]), getComponentValue(vector[3]))
+            addLines(vector, startingPoint)
+
+        def getStartingPoints(vector, point, function, orgVecotr):
+            if len(vector) == 0:
+                if tuple(point) in startingPointSet:
+                    return
+                startingPointSet.add(tuple(point))
+                print(point)
+                function(orgVecotr, point)
+                return
+            if vector[0] == 0:
+                point2 = point.copy()
+                point2.append(0)
+                getStartingPoints(vector[1:], point2, function, orgVecotr)
+                point2 = point.copy()
+                point2.append(1)
+                getStartingPoints(vector[1:], point2, function, orgVecotr)
+                point2 = point.copy()
+                point2.append(2)
+                getStartingPoints(vector[1:], point2, function, orgVecotr)
+            else:
+                point2 = point.copy()
+                point2.append(getComponentValue(vector[0]))
+                getStartingPoints(vector[1:], point2, function, orgVecotr)
+
+        def addSecondaryDiagonalLines(vector):
+            startingPointSet.clear()
+            getStartingPoints(vector, [], addLines, vector)
+
+        # Hlavní diagonály tělesa
+        permutate([1, 1, 1, -1], addDiagonalLines, [])
+        permutate([1, 1, -1, -1], addDiagonalLines, [])
+        permutate([1, -1, -1, -1], addDiagonalLines, [])
+        addLines([1, 1, 1, 1], (0, 0, 0, 0))
+
+        # Doplňkové diagonály tělesa
+        permutate([1, 1, 1, 0], addSecondaryDiagonalLines, [])
+        permutate([1, 1, -1, 0], addSecondaryDiagonalLines, [])
+        permutate([1, 1, 0, 0], addSecondaryDiagonalLines, [])
+        permutate([1, -1, 0, 0], addSecondaryDiagonalLines, [])
+        permutate([1, 0, 0, 0], addSecondaryDiagonalLines, [])
+
+                            
+        
+                       
+
---
+++ matrix.py
@@ -0,0 +1,100 @@
+from math import *
+
+def get_rotation_matrix(axis, rot):
+    '''
+    Returns the 4x4 rotation matrix for a given axis and rotation angle.
+
+    :param axis: the axis to rotate around, can be "x", "y", "z", "w" or "w2"
+    :param rot: the rotation angle in radians
+    :return: the 4x4 rotation matrix
+    '''
+    if axis=="z":
+        return [[cos(rot), -sin(rot),  0 , 0 ],
+                [sin(rot),  cos(rot),  0 , 0 ],
+                [      0 ,        0 ,  1 , 0 ],
+                [      0 ,        0 ,  0 , 1 ]]
+    if axis=="y":
+        return [[ cos(rot),        0 ,  sin(rot),  0 ],
+                [       0 ,        1 ,        0 ,  0 ],
+                [-sin(rot),        0 ,  cos(rot),  0 ],
+                [       0 ,        0 ,        0 ,  1 ]]
+    if axis=="x": 
+        return [[  1 ,      0  ,       0  ,  0 ],
+                [  0 , cos(rot), -sin(rot),  0 ],
+                [  0 , sin(rot),  cos(rot),  0 ],
+                [  0 ,      0  ,       0  ,  1 ]]
+    if axis=="w":
+        return [[ 1 , 0 ,      0  ,       0  ],
+                [ 0 , 1 ,      0  ,       0  ],
+                [ 0 , 0 , cos(rot), -sin(rot)],
+                [ 0 , 0 , sin(rot),  cos(rot)]]
+    if axis=="w2":
+        return [[   1 ,       0 ,        0 ,       0  ],
+                [   0 , cos(rot),        0 ,  sin(rot)],
+                [   0 ,       0 ,        1 ,        0 ],
+                [   0 ,-sin(rot),        0 ,  cos(rot)]]
+
+def get_position_matrix(object):
+    '''
+    Returns the position of an ursina object as a 3x1 matrix.
+
+    :param object: the ursina object to get the position from
+    :return: the 3x1 matrix containing the position of the object
+    '''
+    return [[object.x],
+            [object.y],
+            [object.z]]
+
+def set_position_by_matrix(object, matrix):
+    '''
+    sets the position of an ursina object to the given matrix.
+
+    :param object: the ursina object to be moved
+    :param matrix: the at least 3x1 matrix containing the new position
+    '''
+    object.x = matrix[0][0]
+    object.y = matrix[1][0]
+    object.z = matrix[2][0]
+
+def multiply_matrices(a, b):
+    '''
+    multiplies two given matrices, in case of incompatible dimensions, it prints an error message and returns None.
+
+    :param a: the first matrix at the left side of the multiplication
+    :param b: the second matrix at the right side of the multiplication
+    :return: the resulting matrix of the multiplication
+    '''
+    if len(a[0]) != len(b):
+        print("Matrix multiplication error: incompatible dimensions")
+        return None
+
+    result = []
+    for i in range(len(a)):
+        row = []
+        for j in range(len(b[0])):
+            row.append(sum([a[i][k] * b[k][j] for k in range(len(a[0]))]))
+        result.append(row)
+    return result
+
+def stereographic_projection(lw, coords):
+    ''' 
+    creates the stereographic projection of a 4D point onto a 3D space.
+
+    :param lw: the distance of light source in the 4th dimension
+    :param coords: the 4D coordinates of the point
+    :return: the 3D coordinates vector of the point    
+    '''
+    w = coords[3][0]
+    dist = (lw-(w/3.5))
+    matrix = [[ 1/dist ,    0    ,    0    ,    0 ],
+              [     0    , 1/dist,    0    ,    0 ],
+              [     0    ,    0    , 1/dist,    0 ]]
+    return multiply_matrices(matrix, coords)
+
+def dot(vec1, vec2):
+    # Returns the dot product of two vectors
+    return sum([vec1[i][0]*vec2[i][0] for i in range(len(vec1))])
+
+def getRotation(vec1, vec2):
+    # Returns the angle between two vectors in radians
+    return acos(dot(vec1, vec2)/(norm(vec1)*norm(vec2)))
